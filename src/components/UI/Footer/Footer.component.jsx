7c14ea92484b7e0c88ae9d55b31596221b7211a5
Riz <joshua.riz.layton@gmail.com>
2020-08-14 23:19:34 +0100

    Added social media links and on-hover title effects. Short links need to be adjusted to fit thematically with the rest of the website.

--- src/components/UI/Footer/Footer.component.jsx
+++ src/components/UI/Footer/Footer.component.jsx
@@ -10,6 +10,34 @@ function Footer({ isOpen }) {
     <div className="Footer__container">
       <div className="Footer">
         <h3 className="Footer__container-title">Riz Layton</h3>
+        <div className="Footer__social-media">
+          <div title="GitHub" className="Footer__social-media-item">
+            <span className="fab fa-github-square" />
+          </div>
+          <div title="Stack Overflow" className="Footer__social-media-item">
+            <span className="fab fa-stack-overflow" />
+          </div>
+          <div title="Twitter" className="Footer__social-media-item">
+            <span className="fab fa-twitter" />
+          </div>
+          <div title="Discord" className="Footer__social-media-item">
+            <span className="fab fa-discord" />
+          </div>
+          <div title="Instagram" className="Footer__social-media-item">
+            <span className="fab fa-instagram" />
+          </div>
+        </div>
+        <div className="Footer__short-link-container">
+          <div className="Footer__short-links">
+            <h4>Shop</h4>
+            <ul>
+              <li>Keyboards</li>
+              <li>Artisans</li>
+              <li>Switches</li>
+              <li>Cables</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
